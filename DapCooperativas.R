58b2ad5d33f808c3f4c9e664b16d1ecc93d11a6f
ricardoadley <ricardo.sena@ccc.ufcg.edu.br>
2020-10-24 00:30:48 -0300

    inicio analises com dados DAP

--- DapCooperativas.R
+++ DapCooperativas.R
@@ -10,4 +10,74 @@ DAP_cooperativas <- DAP_cooperativas %>%
 
 #testes
 
-h <- merge(empenho, DAP_cooperativas, by.x = "cnpj_cpf", by.y = "CNPJEspecifico")
+#juntando tabela de empenhos com tabela das cooperativas a partir de
+#cnpj iguais
+
+empenhos_para_cooperativas <- merge(empenho, DAP_cooperativas, by.x = "cnpj_cpf", by.y = "CNPJEspecifico")
+
+#reduzindo campos da tabela apenas para os utilizados nessa analise
+
+empenhos_para_cooperativas <- empenhos_para_cooperativas %>%
+  select(cnpj_cpf,id_contrato,id_orgao,nome_orgao,cd_projeto,nm_projeto,nm_credor,vl_liquidacao,NumeroDeControleExterno,TIPO_DAP)
+
+#Removendo os valores NA das liquidacoes ainda nao efetuadas, substituindo eles por 0
+
+empenhos_para_cooperativas <- empenhos_para_cooperativas  %>%
+  mutate(vl_liquidacao = tidyr::replace_na(vl_liquidacao, 0))
+
+empenhos_para_cooperativas <- empenhos_para_cooperativas  %>%
+  mutate(id_contrato = tidyr::replace_na(id_contrato, 0))
+
+#criando tabela com quantidade de contratos concluidos de cadas prefeitura com
+#cada cooperativa e tambem a quantidade de contratos concluidos totais dessa prefeitura
+#com cooperativas diferentes
+
+contratos_concluidos_com_cooperativas <- empenhos_para_cooperativas %>%
+  group_by(id_orgao) %>%
+  select(id_orgao,nome_orgao,cnpj_cpf,nm_credor,id_contrato) %>%
+  mutate(contr_totais=n_distinct(id_contrato))
+
+#removendo possiveis repeticoes na tabela
+
+contratos_concluidos_com_cooperativas <- unique(contratos_concluidos_com_cooperativas)
+
+#ordenando a tabela pela quantidade de contratos concluidos totais
+contratos_concluidos_com_cooperativas <- contratos_concluidos_com_cooperativas %>%
+  arrange(desc(contr_totais))
+
+contratos_concluidos_com_cooperativas <- contratos_concluidos_com_cooperativas %>%
+  group_by(id_orgao)%>%
+  mutate(total_contratos_com_cooperativa = n_distinct(contr_totais))
+
+#contratos totais da prefeitura com todas as cooperativas
+
+contrato_totais_com_cooperativas <- contratos_concluidos_com_cooperativas %>%
+  select(id_orgao,nome_orgao,contr_totais)
+
+#retirando possiveis repetições
+
+contrato_totais_com_cooperativas <- unique(contrato_totais_com_cooperativas)
+
+#criando tabela com a soma total das liquidacoes direcionadas para
+#cooperativas com DAP por parte da prefeitura
+
+soma_total_liq_orgao_para_cooperativas<- empenhos_para_cooperativas %>%
+  group_by(id_orgao )%>%
+  summarise(soma_liq = sum(vl_liquidacao))
+
+#ordenacao por id das prefeituras
+
+soma_total_liq_orgao_para_cooperativas <- soma_total_liq_orgao_para_cooperativas %>%
+  arrange(desc(id_orgao))
+
+#criando tabela com soma total das liquidacoes feitas pelas prefeitura e
+#com quantas cooperativas com DAP elas tem contrato
+
+contrato_totais_com_cooperativas <- soma_total_liq_orgao_para_cooperativas %>%
+  left_join(contrato_totais_com_cooperativas, by = "id_orgao")
+
+#ordenacao por soma total das liquidacoes da maior para a menor
+
+contrato_totais_com_cooperativas <- contrato_totais_com_cooperativas %>%
+  arrange(desc(soma_liq))
+
